0fddc13aac7d91ce1b038f87006a786492a1cd24
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-04-27 10:44:28 +0200

    created canvas and experimenting with shapes

---
+++ script.js
@@ -0,0 +1,14 @@
+let canvas = document.querySelector('canvas')
+let context = canvas.getContext('2d')
+// console.log(context)
+
+context.fillStyle = '#DFFE72'
+// This is not a fucntion but a property. A property stores a value, like a variable
+context.fillRect(100, 100, 400, 400)
+// This function takes 4 parameters: x, y, (space around the rect) width and height (space inside the rect)
+// fillRect is actually doing 3 things:
+// creating a new shape, drawing a rectangle and filling color
+// If we want to make a more complex shape, we need to give more isntructions
+context.beginPath()
+context.rect()
+context.stroke()
